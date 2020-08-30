c5ed32b081632a7fe070b22ed3b8d655b1788010
kokolem <vitek.peterk@gmail.com>
2020-08-30 20:00:35 +0200

    Initial commit

---
+++ src/main/java/pvpc/pinatakill/PinataKill.java
@@ -0,0 +1,18 @@
+package pvpc.pinatakill;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PinataKill extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        String cmd = getConfig().getString("onPinataKill");
+        getServer().getPluginManager().registerEvents(new PinataKillListener(cmd), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
